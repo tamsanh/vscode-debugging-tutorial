a10e04e26c70addb3350fc484c5e73bab82d6073
Tam Nguyen <tamsanh@gmail.com>
2018-10-22 10:19:02 +0700

    Update the ex3 code

--- src/ex3-express/index.js
+++ src/ex3-express/index.js
@@ -8,13 +8,18 @@ function convert(b) {
   return b
 }
 
-function main(req, res) {
+app.get('/', (req, res) => {
   console.log(convert(req.query.id))
   // Change this output, live, to be 'Hello' when a2 is run
   // Make no code changes
   let output = ""
   res.send(output)
+})
+
+function main() {
+  app.listen(port, () => console.log(`Listening on port ${port}!`))
 }
 
-app.get('/', main)
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+if (typeof module != 'undefined' && !module.parent) {
+  main()
+}
