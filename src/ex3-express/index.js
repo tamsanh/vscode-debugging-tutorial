7238a82db13a4aae3bea51d7d2a8c83fdc41a246
Tam Nguyen <tamsanh@gmail.com>
2018-10-23 07:30:17 +0700

    Remove old comment

--- src/ex3-express/index.js
+++ src/ex3-express/index.js
@@ -10,7 +10,6 @@ function convert(b) {
 
 app.get('/', (req, res) => {
   console.log(convert(req.query.id))
-  // Change this output, live, to be 'Hello' when a2 is run
   // Make no code changes
   let output = ""
   res.send(output)
