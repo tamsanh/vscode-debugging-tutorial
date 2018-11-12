298f2a856ec96bcb8426802eb53b3fca7aebea12
Tam Nguyen <tamsanh@gmail.com>
2018-11-12 14:16:24 +0700

    Fix solution for ex2

--- src/ex2-booleans/solution.js
+++ src/ex2-booleans/solution.js
@@ -25,9 +25,11 @@ function printPerson(person) {
   }
 
   // Can ride an amusement park ride?
-  let canRideAmusementParkRide = person.height > 50
-  canRideAmusementParkRide &= person.weight < 200
-  canRideAmusementParkRide &= person.age > 14
+
+  let isTall = person.height > 50 
+  let isOverweight = !person.weight < 200
+  let isAdult = person.age > 14
+  let canRideAmusementParkRide = isTall && !isOverweight && isAdult
 
   if(canRideAmusementParkRide) {
     console.log(`${person.name} can ride the amusement park ride`)
@@ -35,9 +37,9 @@ function printPerson(person) {
     console.log(`${person.name} can not ride the amusement park ride`)
   }
 
-  let isObese = person.height > 200 && person.weight > 300
-  isObese |= person.height > 50 && person.weight > 100
-  isObese |= person.height > 40 && person.weight > 50
+  let isObese = (person.height > 200 && person.weight > 300)
+    || (person.height > 50 && person.weight > 100)
+    || (person.height > 40 && person.weight > 50);
 
   // Is obese?
   if(isObese) {
